ed8f44dc1c77941baf16b6963dc857e949002ac2
BestToasty <matheoklimke@gmail.com>
2026-02-13 20:30:05 +0100

    added loops

---
+++ projects/loops/target/debug/deps/loops-831ea63d985ffb1b.d
@@ -0,0 +1,5 @@
+/home/toast/rust/projects/loops/target/debug/deps/loops-831ea63d985ffb1b.d: src/main.rs
+
+/home/toast/rust/projects/loops/target/debug/deps/loops-831ea63d985ffb1b: src/main.rs
+
+src/main.rs:
---
+++ projects/loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+/home/toast/rust/projects/loops/target/debug/loops: /home/toast/rust/projects/loops/src/main.rs
